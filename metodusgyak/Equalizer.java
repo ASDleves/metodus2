701e38974116f0996292b049d5aff97a96e218da
ASDleves <ricsi20020302@gmail.com>
2023-02-23 16:21:32 +0100

    lila kockás feladat javitva

--- metodusgyak/Equalizer.java
+++ metodusgyak/Equalizer.java
@@ -10,10 +10,9 @@ public class Equalizer {
 
     private static void eq() {
         Random rnd = new Random();
-        int min = 3, max= 7;
         
         for (int i = 0; i < 5; i++) {
-            int szam = rnd.nextInt(3)+4;
+            int szam = rnd.nextInt(3)+5;
             for (int j = 0; j < szam; j++) {
                 System.out.print("\u001B[45m" + " ");
                 
