357e7fea7fa3f37eb72b5ccbaca68240eaaf3310
ASDleves <ricsi20020302@gmail.com>
2023-02-23 16:03:13 +0100

    metodusokra bontás

--- metodusgyak/MetodusGyak.java
+++ metodusgyak/MetodusGyak.java
@@ -2,16 +2,29 @@ package metodusgyak;
 
 public class MetodusGyak {
     public static void main(String[] args) {
-        int osszeg, gyok;
         kiir("Az első 10 szám összege: "+osszead(elso10szamosszeg(), 0));
+        negyszamosszead();
+        gyokvonas();
+    }
+
+    private static void negyszamosszead() {
+        int osszeg;
         osszeg = osszead(1,2);
         osszeg = osszead(osszeg, 3);
         osszeg = osszead(osszeg, 4);
         kiir("1+2+3+4 ="+osszeg+"\n");
-        gyok = osszead(1, 2);
-        gyok = osszead(gyok, 3);
-        gyok = (gyok**(0.5);
-        kiir("1+2+3 gyöke = "+gyok);
+    }
+
+    private static void gyokvonas() {
+        String kimenet;
+        int osszeg;
+        int a = 3, b =4, c=2;
+        osszeg = osszead(a,b);
+        osszeg = osszead(osszeg, c);
+        String s = "%d+%d+%d gyöke:".format(a,b,c);
+        double gyok = Math.sqrt(osszeg);
+        kimenet = String.format("%s %.4f\n", s, gyok);
+        kiir(kimenet);
     }
     
     
