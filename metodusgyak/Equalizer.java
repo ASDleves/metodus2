4b30a3ad111e5fb2fcb1507fd98b9cfc7d8c8d7a
ASDleves <ricsi20020302@gmail.com>
2023-02-23 16:19:14 +0100

    szinkockák kiirasa

--- metodusgyak/Equalizer.java
+++ metodusgyak/Equalizer.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package metodusgyak;
 
-/**
- *
- * @author Ricsi
- */
+import java.util.Random;
+
 public class Equalizer {
-    
+    public static void main(String[] args) {
+        eq();
+    }
+
+    private static void eq() {
+        Random rnd = new Random();
+        int min = 3, max= 7;
+        
+        for (int i = 0; i < 5; i++) {
+            int szam = rnd.nextInt(3)+4;
+            for (int j = 0; j < szam; j++) {
+                System.out.print("\u001B[45m" + " ");
+                
+            }
+            System.out.println("");
+        }
+        
+    }
 }
